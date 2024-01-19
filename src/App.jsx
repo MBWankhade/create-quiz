7400c1c428f434801b9a03cc41fe7b6d123ecf67
MBWankhade <99143992+MBWankhade@users.noreply.github.com>
2024-01-20 01:21:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Createquiz from './Createquiz';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Createquiz />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
